975485a314b0cc0e1f8b2173fec438bd786bed7f
lay-2025 <iki.engineer2025@gmail.com>
2025-06-12 11:34:36 +0900

    API連携テスト

--- src/routes/api.php
+++ src/routes/api.php
@@ -6,3 +6,24 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+// API test
+Route::get('/list', function () {
+    info('backend call api');
+    return response(
+        [
+            'users' => [
+                [
+                    'id' => '1',
+                    'name' => 'ほげほげ太郎1',
+                    'role' => 'user'
+                ],
+                [
+                    'id' => '2',
+                    'name' => 'ほげほげ太郎2',
+                    'role' => 'user'
+                ],
+            ]
+        ]
+    );
+});
